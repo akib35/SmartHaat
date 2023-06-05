d6f965794634b8b8c6cb0efd29f565e593394d13
Sadman017 <sadmanrahman31@gmail.com>
2023-06-05 11:26:58 +0600

    starting homepage

---
+++ raw_design/smart_haat/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <NavbarView class="index"/>
+    <DashboardView/>
+    <FooterView/>
+    
+  </v-app>
+    
+   
+  
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import NavbarView from '@/components/NavbarView.vue'
+import DashboardView from '@/components/DashboardView.vue'
+import FooterView from '@/components/FooterView.vue'
+export default {
+  name: 'HomeView',
+  components: {
+   // HelloWorld,
+    NavbarView,
+    DashboardView,
+    FooterView
+
+  }
+}
+</script>
